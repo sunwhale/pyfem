b720e309dbde53e50820ddf205500b1b381f7ff2
sunwhale <sunwhale@126.com>
2023-05-07 12:05:41 +0800

    设置Properties属性的访问权限

--- src/pyfem/io/Properties.py
+++ src/pyfem/io/Properties.py
@@ -1,4 +1,4 @@
-from typing import Dict, List, Optional
+from typing import Dict, List
 
 try:
     import tomllib  # type: ignore
@@ -15,64 +15,51 @@ from pyfem.io.Output import Output
 
 
 class Properties:
-    __slots__ = ('__toml', '__title', '__mesh', '__dofs', '__domains', '__materials', '__bcs', '__solver', '__outputs')
+    """
+    Properties类用于解析配置文件中定义的属性。
+    当 self.is_read_only = True 时：
+        1. Properties 类的所有属性在首次被赋非None值后不能再被修改和删除。
+        2. 此时许可的属性关键字存储在self.slots中。
+    """
+    is_read_only = True
+    slots = ('toml', 'title', 'mesh', 'dofs', 'domains', 'materials', 'bcs', 'solver', 'outputs')
 
     def __init__(self):
-        self.__toml = None
-        self.__title = None
-        self.__mesh = None
-        self.__dofs = None
-        self.__domains = None
-        self.__materials = None
-        self.__bcs = None
-        self.__solver = None
-        self.__outputs = None
+        self.toml = None
+        self.title = None
+        self.mesh = None
+        self.dofs = None
+        self.domains = None
+        self.materials = None
+        self.bcs = None
+        self.solver = None
+        self.outputs = None
 
-    @property
-    def toml(self) -> Optional[Dict]:
-        return self.__toml
+    def __setattr__(self, key, value):
+        if self.is_read_only:
+            if key not in self.slots:
+                raise AttributeError(f"{key} is not an allowable attribute keyword")
+            elif hasattr(self, key) and self.__getattribute__(key) is not None:
+                raise PermissionError(f"attribute {key} is read-only")
+            else:
+                super().__setattr__(key, value)
+        else:
+            super().__setattr__(key, value)
 
-    @property
-    def title(self) -> Optional[str]:
-        return self.__title
-
-    @property
-    def mesh(self) -> Optional[Mesh]:
-        return self.__mesh
-
-    @property
-    def dofs(self) -> Optional[Dofs]:
-        return self.__dofs
-
-    @property
-    def domains(self) -> Optional[List[Domain]]:
-        return self.__domains
-
-    @property
-    def materials(self) -> Optional[List[Material]]:
-        return self.__materials
-
-    @property
-    def bcs(self) -> Optional[List[BC]]:
-        return self.__bcs
-
-    @property
-    def solver(self) -> Optional[Solver]:
-        return self.__solver
-
-    @property
-    def outputs(self) -> Optional[List[Output]]:
-        return self.__outputs
+    def __delattr__(self, key):
+        if self.is_read_only:
+            raise PermissionError(f'{self} is read-only')
+        else:
+            super().__delattr__(key)
 
     def show(self) -> None:
         CYAN = '\033[36m'
         MAGENTA = '\033[35m'
         BLUE = '\033[34m'
         END = '\033[0m'
-        for key in self.__slots__:
-            item = self.__getattribute__(f'_Properties{key}')
+        for key, item in self.__dict__.items():
             print()
-            print(CYAN + f'+-{key[2:]}' + END)
+            print(CYAN + f'+-{key}' + END)
             print(MAGENTA + f'  |- {type(item)}' + END)
             if hasattr(item, 'to_string'):
                 print(f'  |- {item.to_string()}')
@@ -83,40 +70,40 @@ class Properties:
                     print(BLUE + f'    |-{i}-{it.to_string(level=3)}' + END)
 
     def set_toml(self, toml: Dict) -> None:
-        self.__toml = toml
+        self.toml = toml
 
     def set_title(self, title: str) -> None:
-        self.__title = title
+        self.title = title
 
     def set_mesh(self, mesh_dict: Dict) -> None:
-        self.__mesh = Mesh()
-        allowed_keys = self.__mesh.__dict__.keys()
+        self.mesh = Mesh()
+        allowed_keys = self.mesh.__dict__.keys()
         for key, item in mesh_dict.items():
             if key in allowed_keys:
-                self.__mesh.__setattr__(key, item)
+                self.mesh.__setattr__(key, item)
             else:
                 raise KeyError(f'{key} is not the keyword of mesh.')
 
     def set_dofs(self, dofs_dict: Dict) -> None:
-        self.__dofs = Dofs()
-        allowed_keys = self.__dofs.__dict__.keys()
+        self.dofs = Dofs()
+        allowed_keys = self.dofs.__dict__.keys()
         for key, item in dofs_dict.items():
             if key in allowed_keys:
-                self.__dofs.__setattr__(key, item)
+                self.dofs.__setattr__(key, item)
             else:
                 raise KeyError(f'{key} is not the keyword of dofs.')
 
     def set_solver(self, solver_dict: Dict) -> None:
-        self.__solver = Solver()
-        allowed_keys = self.__solver.__dict__.keys()
+        self.solver = Solver()
+        allowed_keys = self.solver.__dict__.keys()
         for key, item in solver_dict.items():
             if key in allowed_keys:
-                self.__solver.__setattr__(key, item)
+                self.solver.__setattr__(key, item)
             else:
                 raise KeyError(f'{key} is not the keyword of solver.')
 
     def set_materials(self, materials_list: List) -> None:
-        self.__materials = []
+        self.materials = []
         for material_dict in materials_list:
             material = Material()
             allowed_keys = material.__dict__.keys()
@@ -125,10 +112,10 @@ class Properties:
                     material.__setattr__(key, item)
                 else:
                     raise KeyError(f'{key} is not the keyword of materials.')
-            self.__materials.append(material)
+            self.materials.append(material)
 
     def set_domains(self, domains_list: List) -> None:
-        self.__domains = []
+        self.domains = []
         for domain_dict in domains_list:
             domain = Domain()
             allowed_keys = domain.__dict__.keys()
@@ -137,10 +124,10 @@ class Properties:
                     domain.__setattr__(key, item)
                 else:
                     raise KeyError(f'{key} is not the keyword of domains.')
-            self.__domains.append(domain)
+            self.domains.append(domain)
 
     def set_bcs(self, bcs_list: List) -> None:
-        self.__bcs = []
+        self.bcs = []
         for bc_dict in bcs_list:
             bc = BC()
             allowed_keys = bc.__dict__.keys()
@@ -149,10 +136,10 @@ class Properties:
                     bc.__setattr__(key, item)
                 else:
                     raise KeyError(f'{key} is not the keyword of bcs.')
-            self.__bcs.append(bc)
+            self.bcs.append(bc)
 
     def set_outputs(self, outputs_list: List) -> None:
-        self.__outputs = []
+        self.outputs = []
         for output_dict in outputs_list:
             output = Output()
             allowed_keys = output.__dict__.keys()
@@ -161,60 +148,56 @@ class Properties:
                     output.__setattr__(key, item)
                 else:
                     raise KeyError(f'{key} is not the keyword of bcs.')
-            self.__outputs.append(output)
+            self.outputs.append(output)
 
     def read_file(self, file_name: str) -> None:
         with open(file_name, "rb") as f:
             toml = tomllib.load(f)
             self.set_toml(toml)
 
-        toml_keys = self.__toml.keys()
-        allowed_keys = self.__slots__
+        toml_keys = self.toml.keys()
+        allowed_keys = self.__dict__.keys()
 
         for key in toml_keys:
-            if f'__{key}' not in allowed_keys:
+            if key not in allowed_keys:
                 raise KeyError(f'{key} is not the keyword of properties.')
 
         if 'title' in toml_keys:
-            title = self.__toml['title']
+            title = self.toml['title']
             self.set_title(title)
 
         if 'mesh' in toml_keys:
-            mesh_dict = self.__toml['mesh']
+            mesh_dict = self.toml['mesh']
             self.set_mesh(mesh_dict)
 
         if 'dofs' in toml_keys:
-            dofs_dict = self.__toml['dofs']
+            dofs_dict = self.toml['dofs']
             self.set_dofs(dofs_dict)
 
         if 'domains' in toml_keys:
-            domains_list = self.__toml['domains']
+            domains_list = self.toml['domains']
             self.set_domains(domains_list)
 
         if 'materials' in toml_keys:
-            materials_list = self.__toml['materials']
+            materials_list = self.toml['materials']
             self.set_materials(materials_list)
 
         if 'bcs' in toml_keys:
-            bcs_list = self.__toml['bcs']
+            bcs_list = self.toml['bcs']
             self.set_bcs(bcs_list)
 
         if 'solver' in toml_keys:
-            solver_dict = self.__toml['solver']
+            solver_dict = self.toml['solver']
             self.set_solver(solver_dict)
 
         if 'outputs' in toml_keys:
-            outputs_dict = self.__toml['outputs']
+            outputs_dict = self.toml['outputs']
             self.set_outputs(outputs_dict)
 
 
 if __name__ == "__main__":
     props = Properties()
-    props.show()
-    props.read_file(r'F:\Github\pyfem\examples\rectangle\rectangle.toml')
-    # material = props.materials[0]
-    # print(material.to_string())
-    #
     # props.show()
-
-    props.title = 1
+    props.read_file(r'F:\Github\pyfem\examples\rectangle\rectangle.toml')
+    # props.show()
+    print(props.slots)
