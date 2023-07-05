5268001262fba9bbe5244d459bea6893a616da27
sunwhale <sunwhale@126.com>
2023-07-05 14:08:17 +0800

    修改examples

---
+++ scripts/remove_examples_results_file.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+
+"""
+import os
+
+
+def delete_files_with_extensions(directory, extensions):
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            if any(file.endswith(extension) for extension in extensions):
+                file_path = os.path.join(root, file)
+                os.remove(file_path)
+                print(f"Deleted file: {file_path}")
+
+
+# 指定目录路径和要删除的文件扩展名
+directory_path = "../examples"
+file_extensions = [".pvd", ".vtu"]
+
+# 调用函数来删除文件
+delete_files_with_extensions(directory_path, file_extensions)
