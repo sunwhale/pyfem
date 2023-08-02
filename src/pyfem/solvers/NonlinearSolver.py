a64458af8491630f07e8bc2cb6101c781ad157e2
sunwhale <sunwhale@126.com>
2023-08-02 13:57:08 +0800

    Update NonlinearSolver.py

--- src/pyfem/solvers/NonlinearSolver.py
+++ src/pyfem/solvers/NonlinearSolver.py
@@ -46,18 +46,18 @@ class NonlinearSolver(BaseSolver):
         self.dof_solution = zeros(self.assembly.total_dof_number)
         self.PENALTY: float = 1.0e16
         self.FORCE_TOL: float = 1.0e-6
-        self.MAX_NITER: int = 8
+        self.MAX_NITER: int = 16
 
     def run(self) -> int:
         if self.assembly.props.solver.option in [None, '', 'NR', 'NewtonRaphson']:
-            return self.Newton_Raphson_solve()
+            return self.Newton_Raphson_solve('NR')
         elif self.assembly.props.solver.option in ['IT', 'InitialTangent']:
-            return self.initial_tangent_solve()
+            return self.Newton_Raphson_solve('IT')
         else:
             raise NotImplementedError(error_style(
                 f'unsupported option \'{self.assembly.props.solver.option}\' of {self.assembly.props.solver.type}'))
 
-    def Newton_Raphson_solve(self) -> int:
+    def Newton_Raphson_solve(self, option: str) -> int:
         timer = self.assembly.timer
         timer.total_time = self.solver.total_time
         timer.dtime = self.solver.initial_dtime
@@ -108,7 +108,10 @@ class NonlinearSolver(BaseSolver):
                 da = LU.solve(rhs - fint)
 
                 self.assembly.ddof_solution += da
-                self.assembly.update_element_data()
+                if option == 'NR':
+                    self.assembly.update_element_data()
+                elif option == 'IT':
+                    self.assembly.update_element_data_without_stiffness()
                 self.assembly.assembly_fint()
 
                 f_residual = self.assembly.fext - self.assembly.fint
@@ -132,7 +135,7 @@ class NonlinearSolver(BaseSolver):
                 timer.time0 = timer.time1
                 timer.frame_ids.append(increment)
                 increment += 1
-                timer.dtime *= 1.5
+                timer.dtime *= 1.25
                 if timer.dtime >= self.solver.max_dtime:
                     timer.dtime = self.solver.max_dtime
                 if timer.time0 + timer.dtime >= self.solver.total_time:
@@ -145,97 +148,6 @@ class NonlinearSolver(BaseSolver):
                 self.assembly.goback_element_state_variables()
                 self.assembly.update_element_data()
                 self.assembly.assembly_fint()
-
-            if timer.is_done():
-                write_pvd(self.assembly)
-                break
-
-        if not timer.is_done():
-            print((error_style('maximum increment is reached')))
-            return -1
-        else:
-            return 0
-
-    def initial_tangent_solve(self) -> int:
-        self.MAX_NITER = 1024
-        timer = self.assembly.timer
-
-        timer.total_time = self.solver.total_time
-        timer.dtime = self.solver.initial_dtime
-        timer.time0 = self.solver.start_time
-        timer.increment = 0
-        timer.frame_ids.append(0)
-
-        self.assembly.update_element_field_variables()
-        self.assembly.assembly_field_variables()
-        write_vtk(self.assembly)
-
-        for increment in range(1, self.solver.max_increment):
-
-            timer.time1 = timer.time0 + timer.dtime
-            timer.increment = increment
-
-            print(info_style(f'increment = {increment}, time = {timer.time1}'))
-
-            self.assembly.ddof_solution = zeros(self.assembly.total_dof_number)
-
-            is_convergence = False
-            for niter in range(self.MAX_NITER):
-                if niter == 0:
-                    self.assembly.assembly_global_stiffness()
-                    fint = self.assembly.fint
-                    rhs = deepcopy(self.assembly.fext)
-                    for bc_data in self.assembly.bc_data_list:
-                        amplitude_increment = bc_data.get_amplitude(timer.time1) - bc_data.get_amplitude(timer.time0)
-                        if bc_data.bc.category == 'DirichletBC':
-                            for dof_id, dof_value in zip(bc_data.dof_ids, bc_data.dof_values):
-                                self.assembly.global_stiffness[dof_id, dof_id] += self.PENALTY
-                                rhs[dof_id] += dof_value * self.PENALTY * amplitude_increment
-                        elif bc_data.bc.category == 'NeumannBC':
-                            for dof_id, fext in zip(bc_data.dof_ids, bc_data.bc_fext):
-                                rhs[dof_id] += fext * amplitude_increment
-                                self.assembly.fext[dof_id] += fext * amplitude_increment
-                    LU = splu(self.assembly.global_stiffness)
-                else:
-                    fint = self.assembly.fint
-                    rhs = deepcopy(self.assembly.fext)
-                    for bc_data in self.assembly.bc_data_list:
-                        if bc_data.bc.category == 'DirichletBC':
-                            for dof_id, dof_value in zip(bc_data.dof_ids, bc_data.dof_values):
-                                rhs[dof_id] = 0.0 * self.PENALTY
-
-                da = LU.solve(rhs - fint)
-
-                self.assembly.ddof_solution += da
-                self.assembly.update_element_data_without_stiffness()
-                self.assembly.assembly_fint()
-
-                f_residual = self.assembly.fext - self.assembly.fint
-                f_residual[self.assembly.bc_dof_ids] = 0
-                f_residual = norm(f_residual)
-
-                print(f'  niter = {niter}, force residual = {f_residual}')
-
-                if f_residual < self.FORCE_TOL:
-                    is_convergence = True
-                    break
-
-            if is_convergence:
-                self.assembly.dof_solution += self.assembly.ddof_solution
-                self.assembly.update_element_data()
-                self.assembly.update_element_state_variables()
-                self.assembly.update_element_field_variables()
-                self.assembly.assembly_field_variables()
-
-                write_vtk(self.assembly)
-                timer.time0 = timer.time1
-                timer.frame_ids.append(increment)
-                increment += 1
-            else:
-                increment -= 1
-                timer.dtime *= 0.5
-                self.assembly.goback_element_state_variables()
-                # raise NotImplementedError(error_style('the iteration is not convergence'))
 
             if timer.is_done():
                 write_pvd(self.assembly)
