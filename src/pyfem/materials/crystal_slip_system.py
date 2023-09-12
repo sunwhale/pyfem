b8c6668dc6860f132b46d3b7f52931cf41d665ff
sunwhale <sunwhale@126.com>
2023-09-12 12:07:39 +0800

    Update crystal_slip_system.py

--- src/pyfem/materials/crystal_slip_system.py
+++ src/pyfem/materials/crystal_slip_system.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 r"""
-**提供晶体滑移系的信息**
+**提供晶体变形系统的信息**
 
 本模块中包含3个字典：:py:attr:`slip_dict` ， :py:attr:`cleavage_dict` ， :py:attr:`twin_dict` 。
+存储在字典的返回值为晶体滑移/孪生/解离方向和滑移/孪生/解离面法向组成的元组 (ndarray, ndarray)。
 
 其中 :py:attr:`slip_dict` 字典支持的键值为::
 
@@ -30,48 +31,59 @@ r"""
     bct{211}<01-1>
     bct{211}<-111>
 
-当晶体类型为FCC（）、BCC（）、BCT（）时，存储在字典的返回值为滑移方向和滑移面法向组成的元组 (ndarray, ndarray)。
+其中 :py:attr:`twin_dict` 字典支持的键值为::
 
-对于HCP材料，通常其滑移面和滑移方向都是在非正交的四轴坐标系或三轴坐标系中用密勒指数表示。但在有限元软件中一般使用笛卡尔直角坐标系，所以就有必要将密勒指数转换到笛卡尔直角坐标系中。该过程分为两步进行：
+    fcc{111}<112>
+    bcc{112}<111>
+    hcp{102}<-101>
+    hcp{-1-1-1}<116>
+    hcp{101}<10-2>
+    hcp{112}<11-3>
+
+其中 :py:attr:`cleavage_dict` 字典支持的键值为::
+
+    fcc{001}<001>
+    bcc{001}<001>
+
+当晶体类型为 FCC(Face Centered Cubic)、BCC(Body Centered Cubic)、BCT(Face Centered Tetragonal) 时，
+根据变形系统的类型，返回存储在字典的中的元组 (ndarray, ndarray)，然后再对元组中的元素归一化，获得单位化的晶体滑移/孪生/解离方向和滑移/孪生/解离面法向。
+
+对于 HCP(Hexagonal Close Packed) 结构，通常其滑移面和滑移方向都是在非正交的四轴坐标系或三轴坐标系中用密勒指数表示。
+但在有限元软件中一般使用笛卡尔直角坐标系，所以首先需要将密勒指数转换到笛卡尔直角坐标系中。该过程分为两步进行：
 
 1. 将四轴坐标系中的晶向指数与晶面指数转化到三轴坐标系中：
-设四轴坐标系中晶向指数为[u v t w]，平面指数为[h k i l]，得到：
-三轴坐标系中晶向指数[u1 v1 w1]为：
+
+假设四轴坐标系中晶向指数为 [u v t w]，晶面指数为 [h k i l]，则三轴坐标系中晶向指数 [u1 v1 w1] 和晶面指数 [h0 k0 l0] 用四轴坐标系中的晶向指数与晶面指数表示为::
 
 [u1 v1 w1] = [2u+v u+2v w]
-
-三轴坐标系中晶面指数[h0 h0 h0]为：
-
 [h0 k0 l0] = [h k l]
 
-由于六方系的晶面指数和晶面法向指数不同，根据倒易点阵的相关知识，晶面法向指数[h1 k1 l1]为：
-[h1 k1 l1] = [4h/3+k2/3 h2/3+k4/3 l(a/c)^2]
+由于在三轴坐标系下 HCP 晶体结构的晶面指数和晶面法向指数不同，根据倒易点阵的相关知识，晶面法向指数 [h1 k1 l1] 为::
 
-2. 将三轴坐标系中的晶向指数[u1 v1 w1]和晶面法向指数[h1 k1 l1]转换到直角坐标系，其转化关系如下图所示:
-分别将三轴坐标系的a1轴与直角坐标系的x轴固定，三轴坐标系的c轴与直角坐标系的z轴固定，固定之后，y轴与a2轴之间的夹角为30度。
-其中还用到了晶轴比 r = c/a，完成三轴坐标系c轴与直角坐标系的z轴长度缩放。得到转换公式：
+[h1 k1 l1] = [h*4/3+k*2/3 h*2/3+k*4/3 l*(a/c)^2]
 
-x = a1 - a2*sin(30)  y = a2 * cos(30)  z = r * c
+其中 :math:`c/a` 为晶轴比。
 
-综合第一步的晶向指数[u1 v1 w1]和晶面法向指数[h1 k1 l1]，最后获得直角坐标系中的晶向指数[u2 v2 w2]和晶面法向指数[h2 k2 l2]为:
+2. 将三轴坐标系中的晶向指数 [u1 v1 w1]和晶面法向指数 [h1 k1 l1] 转换到直角坐标系::
 
-[u2 v2 w2] = [3u/2 (u+2v)*sqrt(3)/2 w*(c/a)]
+两种坐标系如下图所示:
 
-[h2 k2 l2] = [h (h+2k)/sqrt(3) l/(c/a)]
+(1)六方晶系的三轴坐标系， :math:`a1,a2` 轴基矢的模 a1 = a2 = a， :math:`c` 轴垂直于a1-a2平面， :math:`a1,a2` 轴的夹角为 :math:`\theta  = {120^ \circ }` ，晶轴比为  :math:`c/a` ::
 
+        c
+        |
+        |
+        |
+        |
+        |
+        o------------------a2
+       /
+      /
+     /
+    /
+  a1
 
-六方晶体的三轴坐标系::
-
-    c
-    |    a2
-    |   /
-    |  /
-    | /
-    |/
-    o--------------- a1
-
-
-六方晶体的直角坐标系::
+(2)六方晶系的直角坐标系::
 
         z
         |
@@ -79,46 +91,68 @@ x = a1 - a2*sin(30)  y = a2 * cos(30)  z = r * c
         |
         |
         |
-        o---------------y
+        o------------------y
        /
       /
      /
-    x
+    /
+   x
 
-整体过程：
+将三轴坐标系的 :math:`a1` 轴与直角坐标系的 :math:`x` 轴固定，三轴坐标系的  :math:`c` 轴与直角坐标系的  :math:`z` 轴固定，固定后， :math:`y` 轴与 :math:`a2` 轴之间的夹角为 :math:`\theta  = {30^ \circ }` 。
+得到转换公式：
+ .. math::
+     x = a1 - a2sin\left( {{{30}^ \circ }} \right),y = a2\cos \left( {{{30}^ \circ }} \right),z = c{\rm{(}}c/a)
 
-direction [uvtw]->[2u+v u+2v w]->[3u/2 (u+2v)*sqrt(3)/2 w*(c/a)]
+综合第一步得到的晶向指数 [u1 v1 w1] 和晶面法向指数 [h1 k1 l1]，最后获得直角坐标系中的晶向指数[u2 v2 w2]和晶面法向指数 [h2 k2 l2] 为::
 
-plane (hkil)->(hkl)>(4/3h+2/3k 2/3h+4/3k l(a/c)^2)->(h (h+2k)/sqrt(3) l/(c/a))
+[u2 v2 w2] = [3u/2 (u+2v)*sqrt(3)/2 w*(c/a)]
+[h2 k2 l2] = [h (h+2k)/sqrt(3) l/(c/a)]
 
+最后对直角坐标系中的晶向指数 [u2 v2 w2] 和晶面法向指数 [h2 k2 l2] 归一化，获得 HCP 结构单位化的晶体滑移/孪生/解离方向和滑移/孪生/解离面法向。
 
-用符号 :math:`\left( {\square ,\square } \right)` 表示两个矢量的内积。
+倒易矩阵相关知识补充::
 
-.. math::
-    \left( {{\mathbf{a}},{{\mathbf{a}}^*}} \right) = 1
+有两种点阵，其中 :math:`{a},{b},{c}` 是真实点阵（正点阵）的点阵参数, :math:`{a^*},{b^*},{c^*}` 为前者的倒易点阵的点阵参数。
 
-补充：
-有两种点阵，a，b，c是真实点阵（正点阵）的点阵参数；a^*, b^*, c^*为前者的倒易点阵的点阵参数。它们存在以下关系：
+用符号 :math:`\left( {\square ,\square } \right)` 表示两个矢量的内积。两种点阵的点阵参数之间存在以下关系：
 
-dot(a, a^*) = 1, dot(b, b^*) = 1, dot(c*, c^*) = 1
+ .. math::
+     \left( {{\mathbf{a}},{{\mathbf{a}}^*}} \right) = \left( {{\mathbf{b}},{{\mathbf{b}}^*}} \right) = \left( {{\mathbf{c}},{{\mathbf{c}}^*}} \right) = 1
 
-dot(a, b^*) = dot(a, c^*) = dot(b, c^*) = dot(b, a^*) = dot(c, a^*) = dot(c, b^*) = 0
+ .. math:: \left( {{\mathbf{a}},{{\mathbf{b}}^*}} \right) = \left( {{\mathbf{a}},{{\mathbf{c}}^*}} \right) = \left( {{\mathbf{b}},{{\mathbf{c}}^*}} \right) = \left( {{\mathbf{b}},{{\mathbf{a}}^*}} \right) = \left( {{\mathbf{c}}, {{\mathbf{a}}^*}} \right) = \left( {{\mathbf{c}},{{\mathbf{b}}^*}} \right) = 0
 
-倒易矩阵形式：Matrix_A = array([[dot(a^*, a^*), dot(a^*, b^*), dot(a^*, c^*)],
-             [dot(b^*, a^*), dot(b^*, b^*), dot(b^*, c^*)],
-             [dot(c^*, a^*), dot(c^*, b^*), dot(c^*, c^*)]])
-
-对于六方晶系，通过查表或者按照上式直接求解，得到a^*, b^*, c^*的长度分别为：2/(aqrt(3)*a),2/(aqrt(3)*b),1/c，此处a = b。
+对于六方晶系，通过查表或者按照上式直接求解，得到 :math:`{a^*},{b^*},{c^*}` 的长度分别为  :math:`2/(\sqrt 3 a),2/(\sqrt 3 b),1/c` ，此处 :math:`a = b` 。
 得到六方晶系的倒易矩阵为：
+ .. math::
+     A = \left[ {\begin{array}{*{20}{c}}
+    {(a,{a^*})}&{(a,{b^*})}&{(a,{c^*})}\\
+    {(b,{a^*})}&{(b,{b^*})}&{(b,{c^*})}\\
+    {(c,{a^*})}&{(c,{b^*})}&{(c,{c^*})}
+    \end{array}} \right]
 
- Matrix_A = array([[4/3/a^2, 2/3/a^2, 0],
-                   [2/3/a^2, 4/3/a^2, 0],
-                [0, 0, 1/c/c]])
-通过倒易矩阵，可以通过平面指数(h k l)得到法线方向指数(h1 k1 l1)。同理，可由法线方向指数(h1 k1 l1)得到平面指数(h k l)。即：
+通过倒易矩阵，可以通过晶面指数 (h k l) 得到晶面法线方向指数 (h1 k1 l1)：
 
-array([h1, k1, l1]) = Matrix_A * array([h1, k1, l1])
+ .. math::
+     \left( \begin{array}{l} h1\\ k1\\ l1 \end{array} \right) = \left[ {\begin{array}{*{20}{c}}
+    {(a,{a^*})}&{(a,{b^*})}&{(a,{c^*})}\\
+    {(b,{a^*})}&{(b,{b^*})}&{(b,{c^*})}\\
+    {(c,{a^*})}&{(c,{b^*})}&{(c,{c^*})}
+    \end{array}} \right]\left( \begin{array}{l} h\\ k\\ l \end{array} \right)
 
-array([h1, k1, l1]) = inv(Matrix_A) * array([h1, k1, l1])
+同理，可由晶面法线方向指数 (h1 k1 l1) 得到晶面指数 (h k l)。即:
+
+ .. math::
+     \left( \begin{array}{l}
+        h\\
+        k\\
+        l \end{array} \right) = {\left[ {\begin{array}{*{20}{c}}
+            {(a,{a^*})}&{(a,{b^*})}&{(a,{c^*})}\\
+            {(b,{a^*})}&{(b,{b^*})}&{(b,{c^*})}\\
+            {(c,{a^*})}&{(c,{b^*})}&{(c,{c^*})} \end{array}} \right]^{ - 1}}\left( \begin{array}{l}
+                h1\\
+                k1\\
+                l1 \end{array} \right)
+参考书：材料科学基础-第2版-余永宁
 """
 
 import re
@@ -638,14 +672,14 @@ def generate_mn(system_type: str, system_name: str, c_over_a: float) -> tuple[in
 
     system_number = len(m_0)
 
-    n_pattern = r"\{(.+?)\}"
-    m_pattern = r"\<(.+?)\>"
-    n_vector = process_string(re.search(n_pattern, system_name).group(1))
-    m_vector = process_string(re.search(m_pattern, system_name).group(1))
+    # n_pattern = r"\{(.+?)\}"
+    # m_pattern = r"\<(.+?)\>"
+    # n_vector = process_string(re.search(n_pattern, system_name).group(1))
+    # m_vector = process_string(re.search(m_pattern, system_name).group(1))
 
     if crystal_type in ['fcc', 'bcc', 'bct']:
-        m = m_0 / norm(m_vector)
-        n = n_0 / norm(n_vector)
+        m = m_0 / norm(m_0, axis=1).reshape((system_number, 1))
+        n = n_0 / norm(n_0, axis=1).reshape((system_number, 1))
 
     elif crystal_type in ['hcp']:
         m = zeros((system_number, 3), dtype=DTYPE)
@@ -659,8 +693,8 @@ def generate_mn(system_type: str, system_name: str, c_over_a: float) -> tuple[in
         n[:, 1] = (n_0[:, 0] + 2.0 * n_0[:, 1]) / sqrt(3.0)
         n[:, 2] = n_0[:, 3] / c_over_a
 
-        m = m / norm(m_vector)
-        n = n / norm(n_vector)
+        m = m / norm(m, axis=1).reshape((system_number, 1))
+        n = n / norm(n, axis=1).reshape((system_number, 1))
 
     else:
         raise NotImplementedError(error_style(
@@ -672,7 +706,7 @@ def generate_mn(system_type: str, system_name: str, c_over_a: float) -> tuple[in
 if __name__ == '__main__':
     system_number, m, n = generate_mn('slip', 'fcc{111}<110>', 1.0)
 
-    system_number, m, n = generate_mn('twin', 'hcp{112}<11-3>', 1.633)
+    # system_number, m, n = generate_mn('twin', 'hcp{112}<11-3>', 1.633)
 
     print('m', m)
     print('n', n)
