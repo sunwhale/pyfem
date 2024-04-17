e1b864ec8d5c6c2bbf93db0b3ea91d16b9a83197
sunwhale <sunwhale@126.com>
2024-04-17 14:35:48 +0800

    增加单元全局坐标形函数梯度dhdx

--- src/pyfem/elements/BaseElement.py
+++ src/pyfem/elements/BaseElement.py
@@ -4,7 +4,7 @@
 """
 from copy import deepcopy
 
-from numpy import dot, ndarray, array, ones, concatenate, transpose
+from numpy import dot, ndarray, array, ones, concatenate, transpose, einsum
 from numpy.linalg import det, inv
 
 from pyfem.fem.Timer import Timer
@@ -27,6 +27,12 @@ class BaseElement:
 
     :ivar iso_element_shape: 等参元对象
     :vartype iso_element_shape: IsoElementShape
+
+    :ivar dimension: 单元空间维度
+    :vartype dimension: int
+
+    :ivar topological_dimension: 单元拓扑维度
+    :vartype topological_dimension: int
 
     :ivar connectivity: 单元节点序号列表
     :vartype connectivity: ndarray
@@ -57,6 +63,9 @@ class BaseElement:
 
     :ivar qp_number: 积分点个数
     :vartype qp_number: int
+
+    :ivar qp_dhdxes: 积分点处的全局坐标形函数梯度
+    :vartype qp_dhdxes: ndarray(qp_number, 空间维度, 单元节点数)
 
     :ivar qp_jacobis: 积分点处的雅克比矩阵列表
     :vartype qp_jacobis: ndarray(qp_number, 空间维度, 空间维度)
@@ -97,6 +106,9 @@ class BaseElement:
     :ivar element_fint: 单元内力列表
     :vartype element_fint: ndarray(element_dof_number,)
 
+    :ivar element_ftime: 单元时间离散外力列表
+    :vartype element_ftime: ndarray(element_dof_number,)
+
     :ivar element_stiffness: 单元刚度矩阵
     :vartype element_stiffness: ndarray(element_dof_number, element_dof_number)
 
@@ -125,6 +137,7 @@ class BaseElement:
         'timer': ('Timer', '计时器对象'),
         'dof_names': ('list[str]', '自由度名称列表'),
         'qp_number': ('int', '积分点个数'),
+        'qp_dhdxes': ('ndarray(qp_number, 空间维度, 单元节点数)', '积分点处的全局坐标形函数梯度'),
         'qp_jacobis': ('ndarray(qp_number, 空间维度, 空间维度)', '积分点处的雅克比矩阵列表'),
         'qp_jacobi_invs': ('ndarray(qp_number,)', '积分点处的雅克比矩阵逆矩阵列表'),
         'qp_jacobi_dets': ('ndarray(qp_number,)', '积分点处的雅克比矩阵行列式列表'),
@@ -138,6 +151,7 @@ class BaseElement:
         'element_dof_values': ('ndarray(element_dof_number,)', '单元全局自由度数值列表'),
         'element_ddof_values': ('ndarray(element_dof_number,)', '单元全局自由度数值增量列表'),
         'element_fint': ('ndarray(element_dof_number,)', '单元内力列表'),
+        'element_ftime': ('ndarray(element_dof_number,)', '单元时间离散外力列表'),
         'element_stiffness': ('ndarray(element_dof_number, element_dof_number)', '单元刚度矩阵'),
         'element_average_field_variables': ('dict[str, ndarray]', '单元磨平后的场变量字典'),
         'allowed_material_data_list': ('list[Tuple]', '许可的单元材料数据类名列表'),
@@ -167,6 +181,7 @@ class BaseElement:
         self.dof_names: list[str] = list()
 
         self.qp_number: int = self.iso_element_shape.qp_number
+        self.qp_dhdxes: ndarray = None  # type: ignore
         self.qp_jacobis: ndarray = None  # type: ignore
         self.qp_jacobi_invs: ndarray = None  # type: ignore
         self.qp_jacobi_dets: ndarray = None  # type: ignore
@@ -182,6 +197,7 @@ class BaseElement:
         self.element_dof_values: ndarray = None  # type: ignore
         self.element_ddof_values: ndarray = None  # type: ignore
         self.element_fint: ndarray = None  # type: ignore
+        self.element_ftime: ndarray = None  # type: ignore
         self.element_stiffness: ndarray = None  # type: ignore
         self.element_average_field_variables: dict[str, ndarray] = dict()
 
@@ -271,6 +287,7 @@ class BaseElement:
             # self.qp_jacobi_invs = []
             # self.qp_jacobi_dets = []
             # for qp_shape_gradient in self.iso_element_shape.qp_shape_gradients:
+            #     print(qp_shape_gradient.shape, self.node_coords.shape)
             #     jacobi = dot(qp_shape_gradient, self.node_coords).transpose()
             #     self.qp_jacobis.append(jacobi)
             #     self.qp_jacobi_invs.append(inv(jacobi))
@@ -280,11 +297,20 @@ class BaseElement:
             # self.qp_jacobi_dets = array(self.qp_jacobi_dets)
 
             # 以下代码为采用numpy高维矩阵乘法的计算方法，计算效率高，但要注意矩阵维度的变化
-            self.qp_jacobis = dot(self.iso_element_shape.qp_shape_gradients, self.node_coords).swapaxes(1, 2)
+            # self.qp_jacobis = dot(self.iso_element_shape.qp_shape_gradients, self.node_coords).swapaxes(1, 2)
+
+            # 以下代码为采用numpy爱因斯坦求和约定函数einsum，更简洁明了
+            self.qp_jacobis = einsum('ijk,kl->ilj', self.iso_element_shape.qp_shape_gradients, self.node_coords)
             self.qp_jacobi_dets = det(self.qp_jacobis)
             # qp_jacobi通常为2×2或3×3的方阵，可以直接根据解析式求逆矩阵，计算效率比numpy.linalg.inv()函数更高
             self.qp_jacobi_invs = inverse(self.qp_jacobis, self.qp_jacobi_dets)
             self.qp_weight_times_jacobi_dets = self.iso_element_shape.qp_weights * self.qp_jacobi_dets
+
+            # qp_dhdxes = []
+            # for iqp, (qp_shape_gradient, qp_jacobi_inv) in enumerate(zip(self.iso_element_shape.qp_shape_gradients, self.qp_jacobi_invs)):
+            #     qp_dhdxes.append(dot(qp_shape_gradient.transpose(), qp_jacobi_inv))
+            # self.qp_dhdxes = array(qp_dhdxes)
+            self.qp_dhdxes = einsum('...ij,...ik->...kj', self.iso_element_shape.qp_shape_gradients, self.qp_jacobi_invs)
 
         elif self.iso_element_shape.coord_type == 'barycentric':
             self.qp_jacobis = dot(self.iso_element_shape.qp_shape_gradients, self.node_coords).swapaxes(1, 2)
