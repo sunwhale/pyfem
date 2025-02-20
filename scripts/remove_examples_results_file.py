92842c9a3aebd8d48fb8af731157de8ced6b90f3
sunwhale <sunwhale@126.com>
2025-02-20 12:29:02 +0800

    Update remove_examples_results_file.py

--- scripts/remove_examples_results_file.py
+++ scripts/remove_examples_results_file.py
@@ -16,12 +16,12 @@ def delete_files_with_extensions(directory, extensions):
 
 # 指定目录路径和要删除的文件扩展名
 directory_path = "../tests/validated_examples"
-file_extensions = [".pvd", ".vtu", ".rpy", ".rec", ".jnl", ".sta", ".log", ".sta"]
+file_extensions = [".pvd", ".vtu", ".rpy", ".rec", ".jnl", ".sta", ".log", ".sta", ".hdf5"]
 # 调用函数来删除文件
 delete_files_with_extensions(directory_path, file_extensions)
 
 # 指定目录路径和要删除的文件扩展名
 directory_path = "../examples"
-file_extensions = [".pvd", ".vtu", ".rpy", ".rec", ".jnl", ".sta", ".log", ".sta"]
+file_extensions = [".pvd", ".vtu", ".rpy", ".rec", ".jnl", ".sta", ".log", ".sta", ".hdf5"]
 # 调用函数来删除文件
 delete_files_with_extensions(directory_path, file_extensions)
