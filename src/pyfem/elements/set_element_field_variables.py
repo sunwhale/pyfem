13a91a54aa3170f89e1ed78a11a80f4299b6422e
sunwhale <sunwhale@126.com>
2025-02-28 15:07:52 +0800

    Update set_element_field_variables.py

--- src/pyfem/elements/set_element_field_variables.py
+++ src/pyfem/elements/set_element_field_variables.py
@@ -19,7 +19,8 @@ def set_element_field_variables(qp_field_variables: dict[str, ndarray], iso_elem
             else:
                 element_nodal_values[key] = dot(iso_element_shape.extrapolated_matrix, item)
         else:
-            element_nodal_values[key] = tile(average(item, axis=0), (iso_element_shape.nodes_number, 1))
+            tile_shape = [iso_element_shape.nodes_number] + [1 for _ in range(len(item.shape) - 1)]
+            element_nodal_values[key] = tile(average(item, axis=0), tile_shape)
 
     element_nodal_field_variables = {}
     for key, item in element_nodal_values.items():
@@ -28,7 +29,6 @@ def set_element_field_variables(qp_field_variables: dict[str, ndarray], iso_elem
                 element_nodal_field_variables['S11'] = item[:, 0]
                 element_nodal_field_variables['S22'] = item[:, 1]
                 element_nodal_field_variables['S12'] = item[:, 2]
-
             elif dimension == 3:
                 element_nodal_field_variables['S11'] = item[:, 0]
                 element_nodal_field_variables['S22'] = item[:, 1]
@@ -36,7 +36,6 @@ def set_element_field_variables(qp_field_variables: dict[str, ndarray], iso_elem
                 element_nodal_field_variables['S12'] = item[:, 3]
                 element_nodal_field_variables['S13'] = item[:, 4]
                 element_nodal_field_variables['S23'] = item[:, 5]
-
             element_nodal_field_variables['S'] = item
 
         if key == 'strain':
@@ -44,7 +43,6 @@ def set_element_field_variables(qp_field_variables: dict[str, ndarray], iso_elem
                 element_nodal_field_variables['E11'] = item[:, 0]
                 element_nodal_field_variables['E22'] = item[:, 1]
                 element_nodal_field_variables['E12'] = item[:, 2]
-
             elif dimension == 3:
                 element_nodal_field_variables['E11'] = item[:, 0]
                 element_nodal_field_variables['E22'] = item[:, 1]
@@ -52,7 +50,6 @@ def set_element_field_variables(qp_field_variables: dict[str, ndarray], iso_elem
                 element_nodal_field_variables['E12'] = item[:, 3]
                 element_nodal_field_variables['E13'] = item[:, 4]
                 element_nodal_field_variables['E23'] = item[:, 5]
-
             element_nodal_field_variables['E'] = item
 
         if key == 'energy':
@@ -65,6 +62,7 @@ def set_element_field_variables(qp_field_variables: dict[str, ndarray], iso_elem
                 element_nodal_field_variables['HFL2'] = item[:, 1]
             if dimension >= 3:
                 element_nodal_field_variables['HFL3'] = item[:, 2]
+            element_nodal_field_variables['HFL'] = item
 
         if key == 'concentration_flux':
             if dimension >= 1:
@@ -73,6 +71,10 @@ def set_element_field_variables(qp_field_variables: dict[str, ndarray], iso_elem
                 element_nodal_field_variables['CFL2'] = item[:, 1]
             if dimension >= 3:
                 element_nodal_field_variables['CFL3'] = item[:, 2]
+            element_nodal_field_variables['CFL'] = item
+
+        if 'SDV' in key:
+            element_nodal_field_variables[key] = item
 
     return element_nodal_field_variables
 
