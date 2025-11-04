156d0b7ff16edb683ae60f7b50750a16952ee923
sunwhale <sunwhale@126.com>
2025-11-04 10:36:29 +0800

    Update TabularAmplitude.py

--- src/pyfem/amplitude/TabularAmplitude.py
+++ src/pyfem/amplitude/TabularAmplitude.py
@@ -4,8 +4,8 @@
 """
 from typing import Callable
 
-from numpy import array, all, diff, ndarray
-from scipy.interpolate import interp1d  # type: ignore
+import numpy as np
+import scipy as sp
 
 from pyfem.amplitude.BaseAmplitude import BaseAmplitude
 from pyfem.io.Amplitude import Amplitude
@@ -29,15 +29,14 @@ class TabularAmplitude(BaseAmplitude):
     def __init__(self, amplitude: Amplitude) -> None:
         super().__init__()
         self.start = amplitude.start
-        self.table: ndarray = array(amplitude.data)
+        self.table: np.ndarray = np.array(amplitude.data)
         if self.table.ndim != 2:
             raise NotImplementedError(error_style('dimension of amplitude table must be 2'))
         elif self.table.shape[1] != 2:
             raise NotImplementedError(error_style('column of amplitude table must be 2'))
-        elif not all(diff(self.table[:, 0]) > 0):
+        elif not all(np.diff(self.table[:, 0]) > 0):
             raise NotImplementedError(error_style('time of amplitude table must be monotonic increase'))
-        self.f_amplitude: Callable = interp1d(self.table[:, 0], self.table[:, 1], kind='linear',
-                                              fill_value='extrapolate')
+        self.f_amplitude: Callable = sp.interpolate.interp1d(self.table[:, 0], self.table[:, 1], kind='linear', fill_value='extrapolate')
 
     def get_amplitude(self, time: float) -> float:
         return self.f_amplitude(time) + self.start
