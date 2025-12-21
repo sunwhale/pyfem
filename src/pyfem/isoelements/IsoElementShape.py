e76559adce53b5c8a53627308f27badc95dbc184
sunwhale <sunwhale@126.com>
2025-12-22 01:04:58 +0800

    Update IsoElementShape.py

--- src/pyfem/isoelements/IsoElementShape.py
+++ src/pyfem/isoelements/IsoElementShape.py
@@ -533,32 +533,59 @@ class IsoElementShape:
 
     def set_hex20(self) -> None:
         self.coord_type = 'cartesian'
+        self.element_geo_type = 'hex20'
         self.dimension = 3
         self.topological_dimension = 3
-        self.nodes_number = 20
-        self.nodes_number_independent = self.nodes_number
-        self.order = 3
-        quadrature = GaussLegendreQuadrature(order=self.order, dimension=self.dimension)
-        self.qp_coords, self.qp_weights = quadrature.get_quadrature_coords_and_weights()
-        self.shape_function = get_shape_hex20
 
-        self.bc_surface_number = 6
+        self.order_standard = 3
+        self.order = 3
+
+        self.nodes = np.array([[-1.0, -1.0, -1.0],
+                               [1.0, -1.0, -1.0],
+                               [1.0, 1.0, -1.0],
+                               [-1.0, 1.0, -1.0],
+                               [-1.0, -1.0, 1.0],
+                               [1.0, -1.0, 1.0],
+                               [1.0, 1.0, 1.0],
+                               [-1.0, 1.0, 1.0],
+                               [0.0, -1.0, -1.0],
+                               [1.0, 0.0, -1.0],
+                               [0.0, 1.0, -1.0],
+                               [-1.0, 0.0, -1.0],
+                               [0.0, -1.0, 1.0],
+                               [1.0, 0.0, 1.0],
+                               [0.0, 1.0, 1.0],
+                               [-1.0, 0.0, 1.0],
+                               [-1.0, -1.0, 0.0],
+                               [1.0, -1.0, 0.0],
+                               [1.0, 1.0, 0.0],
+                               [-1.0, 1.0, 0.0]], dtype=DTYPE)
+        self.edges = np.array([[0, 1, 8],
+                               [1, 2, 9],
+                               [2, 3, 10],
+                               [3, 0, 11],
+                               [4, 5, 12],
+                               [5, 6, 13],
+                               [6, 7, 14],
+                               [7, 0, 15],
+                               [0, 4, 16],
+                               [1, 5, 17],
+                               [2, 6, 18],
+                               [3, 7, 19]], dtype='int32')
+        self.faces = np.array([[0, 3, 7, 4, 11, 19, 15, 16],
+                               [1, 2, 6, 5, 9, 18, 13, 17],
+                               [0, 1, 5, 4, 8, 17, 12, 16],
+                               [3, 2, 6, 7, 10, 18, 14, 19],
+                               [0, 1, 2, 3, 8, 9, 10, 11],
+                               [4, 5, 6, 7, 12, 13, 14, 15]], dtype='int32')
+        self.cells = np.array([[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]], dtype='int32')
+
+        quadrature = GaussLegendreQuadrature(order=self.order, dimension=self.dimension)
         bc_quadrature = GaussLegendreQuadrature(order=self.order, dimension=self.dimension - 1)
-        bc_qp_coords, self.bc_qp_weights = bc_quadrature.get_quadrature_coords_and_weights()
-        self.bc_surface_nodes_dict = {'s1': (0, 11, 3, 19, 7, 15, 4, 16),
-                                      's2': (1, 9, 2, 18, 6, 13, 5, 17),
-                                      's3': (0, 8, 1, 17, 5, 12, 4, 16),
-                                      's4': (3, 10, 2, 18, 6, 14, 7, 19),
-                                      's5': (0, 8, 1, 9, 2, 10, 3, 11),
-                                      's6': (4, 12, 5, 13, 6, 14, 7, 15)}
-        self.bc_surface_coord_dict = {'s1': (0, -1, -1, 1),
-                                      's2': (0, 1, 1, 1),
-                                      's3': (1, -1, 1, 1),
-                                      's4': (1, 1, -1, 1),
-                                      's5': (2, -1, -1, 1),
-                                      's6': (2, 1, 1, 1)}
-        self.bc_qp_coords_dict = {name: np.insert(bc_qp_coords, item[0], item[1], axis=1) for name, item in
-                                  self.bc_surface_coord_dict.items()}
+        bc_surface_direction_weight = [[-1, 1], [1, 1], [1, 1], [-1, 1], [-1, 1], [1, 1]]
+
+        self.set_cell(quadrature)
+        self.set_bc(bc_quadrature, bc_surface_direction_weight)
         self.diagram = IsoElementDiagram.hex20
 
     @staticmethod
